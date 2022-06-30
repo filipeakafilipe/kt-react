ed54a88233c29dc4cadbbbd37be124080ebd55b4
Filipe <filipeffb99@gmail.com>
2022-06-29 23:26:22 -0300

    add useMemo example

---
+++ usememo-demo/src/App.js
@@ -0,0 +1,11 @@
+import { UmComponente } from './UmComponente';
+
+function App() {
+	return (
+		<>
+			<UmComponente />
+		</>
+	);
+}
+
+export default App;
---
+++ usememo-demo/src/UmComponente.js
@@ -0,0 +1,35 @@
+import React, { useState, useMemo } from 'react';
+
+export const UmComponente = () => {
+    const [estadoParaAtualizarSomenteOComponente, setEstadoParaAtualizarSomenteOComponente] = useState(false);
+    const funcaoParaAtualizarSomenteOComponente = () => {
+        setEstadoParaAtualizarSomenteOComponente(prevState => !prevState);
+    }
+
+    const [estadoParaAtualizarOComponenteEValorComplexo, setEstadoParaAtualizarOComponenteEValorComplexo] = useState(false);
+    const funcaoParaAtualizarOComponenteEValorComplexo = () => {
+        setEstadoParaAtualizarOComponenteEValorComplexo(prevState => !prevState);
+    }
+    
+    const funcaoComputacionalmentePesada = () => {
+        var start = new Date().getTime();
+        var end = 0;
+        while((end-start) < 2000){
+            end = new Date().getTime();
+        }
+
+        return Math.floor(Math.random() * 101);
+    }
+
+    const umValorComplexo = useMemo(() => {
+        return funcaoComputacionalmentePesada();
+    }, [estadoParaAtualizarOComponenteEValorComplexo]);
+
+    return (
+        <>
+            <button onClick={() => funcaoParaAtualizarOComponenteEValorComplexo()}>Atualizar o Componente e Valor Complexo</button>
+            <button onClick={() => funcaoParaAtualizarSomenteOComponente()}>Atualizar Somente o Componente</button>
+            <div>{umValorComplexo}</div>
+        </>
+    )
+}
