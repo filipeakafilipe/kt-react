b5da7ed819181f8516546d933e5f1f1912dca7c6
Filipe <filipeffb99@gmail.com>
2022-06-30 08:44:07 -0300

    add initial profiler-demo version

--- profiler-demo/src/App.js
+++ profiler-demo/src/App.js
@@ -4,16 +4,9 @@ const Item = ({ text }) => {
     return <li>{text}</li>;
 }
 
-const Message = () => {
+const List = () => {
     const [message, setMessage] = useState('');
 
-    return (<>
-        <input value={message} onChange={e => setMessage(e.target.value)} />
-        <button onClick={() => alert(message)}>Adicionar Mensagem</button>
-    </>);
-};
-
-const List = () => {
     let itens = [];
 
     for (let index = 0; index < 50; index++) {
@@ -22,7 +15,8 @@ const List = () => {
 
     return (
         <>
-            <Message />
+            <input value={message} onChange={e => setMessage(e.target.value)} />
+            <button onClick={() => alert(message)}>Adicionar Mensagem</button>
             <ul>
                 {Array.from(Array(50), (el, index) => <Item text={`${index}`} />)}
             </ul>
